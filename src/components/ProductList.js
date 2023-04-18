29d610affd020fe5f9de9044b953b94d95b21720
Marcelo Di Jorge <marcelobdij@gmail.com>
2023-04-18 19:33:29 -0300

    Initial commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get("http://localhost:5000/api/products");
+                setProducts(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    return (
+        <div>
+            <h1>Product List</h1>
+            <ul>
+                {products.map((product) => (
+                    <li key={product._id}>
+                        <h2>{product.name}</h2>
+                        <p>{product.description}</p>
+                        <p>Price: ${product.price}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ProductList;
